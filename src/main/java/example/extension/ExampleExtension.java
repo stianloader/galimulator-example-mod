b708455f49dca46dd11873bfb64ced0eaa415f98
Geolykt <admin@geolykt.de>
2025-08-28 14:05:47 +0200

    Update to gradle 9, update dependencies

--- src/main/java/example/extension/ExampleExtension.java
+++ src/main/java/example/extension/ExampleExtension.java
@@ -1,13 +1,14 @@
 package example.extension;
 
 import de.geolykt.starloader.api.event.EventManager;
+import de.geolykt.starloader.api.event.lifecycle.SignalExtensionTerminationEvent;
 import de.geolykt.starloader.mod.Extension;
 
 public class ExampleExtension extends Extension {
 
     @Override
     public void initialize() {
-        getLogger().info("Hello from the example plugin!");
+        this.getLogger().info("Hello from the example plugin!");
         EventManager.registerListener(new ExampleListener());
     }
 
@@ -16,5 +17,13 @@ public class ExampleExtension extends Extension {
         // Please be very careful when implementing the terminate function
         // If you are not careful and a deadlock occurs, then the VM might deadlock too
         // As such, do not rely on any other threads in the terminate function and code everything async-safe
+
+        // Instead use SLAPI's ApplicationStopEvent which fires most of the time.
+
+        // The next line is optional, but it signals to others that your extension is soon no longer available.
+        // Note that SLAPI calls this event in the unload() method, however we cannot do that as by that time
+        // SLAPI (which handles the event) would be unavailable at that point in time. It's a unique thing
+        // that works out for SLAPI and SLAPI only.
+        EventManager.handleEvent(new SignalExtensionTerminationEvent(this));
     }
 }
--- src/main/java/example/extension/ExampleListener.java
+++ src/main/java/example/extension/ExampleListener.java
@@ -1,7 +1,7 @@
 package example.extension;
 
 import de.geolykt.starloader.api.Galimulator;
-import de.geolykt.starloader.api.empire.ActiveEmpire;
+import de.geolykt.starloader.api.dimension.Empire;
 import de.geolykt.starloader.api.event.EventHandler;
 import de.geolykt.starloader.api.event.EventPriority;
 import de.geolykt.starloader.api.event.Listener;
@@ -12,11 +12,11 @@ public class ExampleListener implements Listener {
 
     @EventHandler(EventPriority.MONITOR)
     public void onAllianceJoin(AllianceJoinEvent event) {
-        if (event.getAlliance().getMembers().size() > 2) { // These are not the founders
-            ActiveEmpire playerEmpire = Galimulator.getPlayerEmpire();
-            if (playerEmpire != null && event.getEmpire() != playerEmpire) { // Not the player empire
+        if (event.getAlliance().getMemberView().size() > 2) { // These are not the founders
+            Empire playerEmpire = Galimulator.getUniverse().getPlayerEmpire();
+            if (playerEmpire != null && event.getJoiningEmpire() != playerEmpire) { // Not the player empire
                 if (event.getAlliance().hasEmpire(playerEmpire)) { // The player is in the alliance
-                    BasicDialogBuilder dialogBuilder = new BasicDialogBuilder("Empire joins Alliance", event.getEmpire().getEmpireName() + " has joined your alliance.");
+                    BasicDialogBuilder dialogBuilder = new BasicDialogBuilder("Empire joins Alliance", event.getJoiningEmpire().getEmpireName() + " has joined your alliance.");
                     dialogBuilder.setDuration(3); // Only show for 3 seconds maximum
                     dialogBuilder.show(); // Show dialog
                 }
